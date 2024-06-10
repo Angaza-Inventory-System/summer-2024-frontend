295bc6a7f6d6cbf4dfe29f924ab98377da21ed46
Connor Lin <connorlin06@gmail.com>
2024-06-10 12:56:55 -0500

    Test 2

--- backend/angaza_inventory_system/asgi.py
+++ backend/angaza_inventory_system/asgi.py
@@ -14,4 +14,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'angaza_inventory_system.settings')
 
 application = get_asgi_application()
-import time
+import os
