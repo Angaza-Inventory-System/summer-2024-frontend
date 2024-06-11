b64cbc8ad7cfc63bfd0b34eeda290270571935b5
Connor Lin <connorlin06@gmail.com>
2024-06-10 20:55:06 -0500

    Convert repo to frontend only

--- backend/angaza_inventory_system/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-
-urlpatterns = [
-    path("auth/", include("angaza_inventory_system.auth.urls")),
-    path("devices/", include("angaza_inventory_system.devices.urls")),
-    path("users/", include("angaza_inventory_system.users.urls")),
-]
