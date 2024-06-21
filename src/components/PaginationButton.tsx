20eff26fc0af7fa29551a6be571f496893afbc6a
dliu25 <161968648+dliu25@users.noreply.github.com>
2024-06-20 19:04:29 -0500

    column sizing

--- src/components/PaginationButton.tsx
+++ src/components/PaginationButton.tsx
@@ -1,55 +1,22 @@
 import PaginationNumber from "./PaginationNumbers";
-
-export function PaginationButton() {
+interface Props {
+  table: any;
+  index: number;
+  pageCount: number;
+}
+export function PaginationButton({ table, index, pageCount }: Props) {
+  console.log(typeof pageCount);
   return (
-    <>
-      <nav aria-label="Page navigation example">
-        <ul className="flex h-8 items-center -space-x-px text-sm">
-          <li>
-            <div className="ms-0 flex h-8 items-center justify-center rounded-s-lg border border-e-0 border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-              <span className="sr-only">Previous</span>
-              <svg
-                className="h-2.5 w-2.5 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M5 1 1 5l4 4"
-                />
-              </svg>
-            </div>
-          </li>
-          <PaginationNumber text="1" />
-          <PaginationNumber text="2" />
-          <PaginationNumber text="3" />
-          <li>
-            <div className="flex h-8 items-center justify-center rounded-e-lg border border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-              <span className="sr-only">Next</span>
-              <svg
-                className="h-2.5 w-2.5 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m1 9 4-4-4-4"
-                />
-              </svg>
-            </div>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <div className="flex h-8 items-center -space-x-px text-sm">
+      <div className="ms-0 flex h-8 items-center justify-center rounded-s-lg border border-e-0 border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        {"<"}
+      </div>
+      <PaginationNumber text={String(index + 1)} />
+      <PaginationNumber text="..." />
+      <PaginationNumber text={String(pageCount)} />
+      <div className="flex h-8 items-center justify-center rounded-e-lg border border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        {">"}
+      </div>
+    </div>
   );
 }
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,31 +1,17 @@
+import React, { useState, useMemo } from "react";
 import {
   flexRender,
   getCoreRowModel,
   useReactTable,
   getPaginationRowModel,
+  ColumnDef,
+  ColumnHelper,
 } from "@tanstack/react-table";
-import { useState } from "react";
-import dataJSON from "./mockdata.json";
-import { createColumnHelper } from "@tanstack/react-table";
 import Checkbox from "./Checkbox";
 import { Dropdown } from "flowbite-react";
 import { PaginationButton } from "./PaginationButton";
-
-export type ColumnSizingTableState = {
-  columnSizing: ColumnSizing;
-  columnSizingInfo: ColumnSizingInfoState;
-};
-
-export type ColumnSizing = Record<string, number>;
-
-export type ColumnSizingInfoState = {
-  startOffset: null | number;
-  startSize: null | number;
-  deltaOffset: null | number;
-  deltaPercentage: null | number;
-  isResizingColumn: false | string;
-  columnSizingStart: [string, number][];
-};
+import dataJSON from "./mockdata.json";
+import { createColumnHelper } from "@tanstack/react-table";
 
 function Table() {
   const [rows, setRows] = useState(dataJSON);
@@ -43,10 +29,18 @@ function Table() {
     "first_name",
     "last_name",
   ]);
-  const columnHelper = createColumnHelper();
-  const colDef = [
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 16,
+  });
+
+  const columnHelper: ColumnHelper<any> = createColumnHelper();
+
+  const colDef: ColumnDef<any>[] = [
     {
       id: "select",
+      size: 52,
+      enableResizing: false,
       header: ({ table }) => (
         <Checkbox
           {...{
@@ -68,22 +62,21 @@ function Table() {
       ),
     },
     columnHelper.accessor("id", {
+      minSize: 100,
       header: "ID",
       cell: (info) => info.getValue(),
     }),
     columnHelper.accessor("first_name", {
+      minSize: 150,
       header: "First Name",
       cell: (info) => info.getValue(),
     }),
     columnHelper.accessor("last_name", {
+      minSize: 150,
       header: "Last Name",
       cell: (info) => info.getValue(),
     }),
   ];
-  const [pagination, setPagination] = useState({
-    pageIndex: 0,
-    pageSize: 16, //subject to change after asking backend
-  });
 
   const table = useReactTable({
     data: rows,
@@ -93,8 +86,8 @@ function Table() {
     getPaginationRowModel: getPaginationRowModel(),
     manualPagination: true,
     onPaginationChange: setPagination,
-    autoResetPageIndex: false, //turn off auto reset of pageIndex
-    rowCount: 16, //hard coded to maintain order
+    autoResetPageIndex: false, // turn off auto reset of pageIndex
+    pageCount: -1,
     state: {
       rowSelection,
       columnOrder,
@@ -106,71 +99,111 @@ function Table() {
     onColumnOrderChange: setColumnOrder,
     enableRowSelection: true,
   });
+
+  const columnSizeVars = useMemo(() => {
+    const headers = table.getFlatHeaders();
+    const colSizes: { [key: string]: number } = {};
+    headers.forEach((header) => {
+      colSizes[`--header-${header.id}-size`] = header.getSize();
+      colSizes[`--col-${header.column.id}-size`] = header.column.getSize();
+    });
+    return colSizes;
+  }, [table.getState().columnSizingInfo, table.getState().columnSizing]);
+
   return (
-    <div>
-      <Dropdown label="Hide Columns" dismissOnClick={false} size="sm">
-        {table.getAllLeafColumns().map((column) => {
-          if (column.id == "select") return;
-          return (
-            <div
-              key={column.id}
-              className="flex items-center hover:bg-slate-200"
-            >
-              <label>
-                <input
-                  {...{
-                    type: "checkbox",
-                    checked: column.getIsVisible(),
-                    onChange: column.getToggleVisibilityHandler(),
-                    className:
-                      "h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700",
-                  }}
-                />
-                <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                  {column.columnDef.header}
+    <div className="w-screen p-2">
+      <div className="pb-3">
+        <Dropdown label="Hide Columns" dismissOnClick={false} size="sm">
+          {table.getAllLeafColumns().map((column) => {
+            if (column.id === "select") return null;
+            return (
+              <div
+                key={column.id}
+                className="flex items-center hover:bg-slate-200"
+              >
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    checked={column.getIsVisible()}
+                    onChange={column.getToggleVisibilityHandler()}
+                    className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
+                  />
+                  <span className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                    {typeof column.columnDef.header === "function"
+                      ? flexRender(column.columnDef.header, { column })
+                      : column.columnDef.header}
+                  </span>
                 </label>
-              </label>
-            </div>
-          );
-        })}
-      </Dropdown>
+              </div>
+            );
+          })}
+        </Dropdown>
+      </div>
       <div className="relative w-full overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right">
-          <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+        <div
+          className="w-full text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right"
+          style={{ ...columnSizeVars }}
+        >
+          <div className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
             {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
+              <div key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
-                  <th key={header.id} className="px-6 py-3">
-                    <div className="flex items-center">
-                      {flexRender(
-                        header.column.columnDef.header,
-                        header.getContext(),
-                      )}
-                    </div>
-                  </th>
+                  <div
+                    key={header.id}
+                    className="relative table-cell bg-gray-50 px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-700 dark:bg-gray-700 dark:text-gray-400"
+                    style={{
+                      width: `calc(var(--header-${header.id}-size) * 1px)`,
+                    }}
+                  >
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+                    <div
+                      onDoubleClick={() => header.column.resetSize()}
+                      onMouseDown={header.getResizeHandler()}
+                      onTouchStart={header.getResizeHandler()}
+                      className="absolute right-0 top-0 h-full w-1 cursor-col-resize select-none bg-blue-100 hover:bg-blue-500"
+                    />
+                  </div>
                 ))}
-              </tr>
+              </div>
             ))}
-          </thead>
-          <tbody>
+          </div>
+          <div>
             {table.getRowModel().rows.map((row) => (
-              <tr
+              <div
                 key={row.id}
                 className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
-                onClick={row.getToggleSelectedHandler()}
               >
                 {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id} className="px-6 py-4">
+                  <div
+                    key={cell.id}
+                    className="table-cell px-6 py-4"
+                    style={{
+                      width: `calc(var(--col-${cell.column.id}-size) * 1px)`,
+                    }}
+                  >
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
+                  </div>
                 ))}
-              </tr>
+              </div>
             ))}
-          </tbody>
-        </table>
+          </div>
+        </div>
       </div>
-      <PaginationButton />
+      <div className="grid w-full grid-cols-2 pt-3">
+        <div></div>
+        <div className="justify-self-end">
+          <PaginationButton
+            table={table}
+            index={pagination.pageIndex}
+            pageCount={table.getPageCount()}
+          />
+        </div>
+      </div>
     </div>
   );
 }
+
 export default Table;
